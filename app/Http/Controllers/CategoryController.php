b8981687427907d3b6efa3a3e08ed4659b666f5b
ShwetaEchake <shwetaechake16@gmail.com>
2025-05-24 15:01:36 +0530

    name change

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,7 +23,7 @@ class CategoryController extends Controller
              $category = Category::create($data);
 
              return response()->json([
-                    'message' => 'Post added successfully',
+                    'message' => 'Category added successfully',
                     'category' => $category
                 ],201);
 
